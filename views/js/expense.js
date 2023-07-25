9231f7e28feccfe5bc0f0eeeacfd01ef59aca545
adnan291 <mohd.adnan.627492@gmail.com>
2023-07-25 16:04:51 +0530

    backend ready

--- views/js/expense.js
+++ views/js/expense.js
@@ -107,17 +107,18 @@ async function saveToDatabase(event) {
       document.getElementById('description').value = '';
      document.getElementById('category').value = '';
      document.getElementById('expenseAmount').value = '';
+// console.log("Description" ,expense.description);
   
   
      const parentNode = document.getElementById('expenses');
      const childHTML = ` <li id=${expense.id}> ${expense.description} - ${expense.expenseAmount}
      <div class="expense-buttons">
-     <button class="delete" onclick=deleteExpense('${expense.id}')><span class="text">Delete</span><span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path></svg></span></button>
-     <button class="edit" onclick=editExpenseDetails('${expense.description}','${expense.expenseAmount}','${expense.category}','${expense.id}')><span class="text">Edit</span><span class="icon"><svg class="svg" viewBox="0 0 512 512"><path d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"></path></svg></span></button>
+     <button class="delete" onclick="deleteExpense('${expense.id}')"><span class="text">Delete</span><span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path></svg></span></button>
                                    </div></li>`;
   
                                    parentNode.innerHTML = parentNode.innerHTML + childHTML;
-                                   console.log("showNewExpenseOnScreen",parentNode.parentNode);
+                                  //  <button class="edit" onclick="editExpenseDetails('${expense.description}','${expense.expenseAmount}','${expense.category}','${expense.id}')"><span class="text">Edit</span><span class="icon"><svg class="svg" viewBox="0 0 512 512"><path d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"></path></svg></span></button>
+                                   //  console.log("showNewExpenseOnScreen",parentNode.parentNode);
    }
 
     function showDMYExpense(expense, ulid) {
@@ -132,8 +133,8 @@ async function saveToDatabase(event) {
      </li>`;
   
     //  <div class="expense-buttons">
-    //  <button class="delete" onclick=deleteExpense('${expense.id}')><span class="text">Delete</span><span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path></svg></span></button>
-    //  <button class="edit" onclick=editExpenseDetails('${expense.description}','${expense.expenseAmount}','${expense.category}','${expense.id}')><span class="text">Edit</span><span class="icon"><svg class="svg" viewBox="0 0 512 512"><path d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"></path></svg></button>
+    //  <button class="delete" onclick="deleteExpense('${expense.id}')"><span class="text">Delete</span><span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path></svg></span></button>
+    //  <button class="edit" onclick="editExpenseDetails('${expense.description}','${expense.expenseAmount}','${expense.category}','${expense.id}')"><span class="text">Edit</span><span class="icon"><svg class="svg" viewBox="0 0 512 512"><path d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"></path></svg></button>
     //                                </div>
 
      parentNode.innerHTML = parentNode.innerHTML + childHTML;
@@ -303,8 +304,8 @@ async function saveToDatabase(event) {
 
   async function getExpense(page, expPerPage) {
     try {
-      console.log("expPerPage",expPerPage);
-      console.log("page",page);
+      // console.log("expPerPage",expPerPage);
+      // console.log("page",page);
       const res = await axios.get(`http://localhost:4000/expense/get-expense?page=${page}&expPerPage=${expPerPage}`,{ headers: { Authorization: token } });
 
       parentNode.innerHTML = "";
@@ -353,6 +354,7 @@ async function saveToDatabase(event) {
       selectPerPage.innerHTML =  `<div id="pagelist">
       <label for="price">Rows Per Page</label>
       <select name="price" id="price" onchange="rowChange(event)">
+        <option value=''></option>
         <option value='2'>2</option>
         <option value='4'>4</option>
         <option value='6'>6</option>
@@ -370,6 +372,7 @@ async function saveToDatabase(event) {
   
   function editExpenseDetails(description,expenseAmount,category,expenseId){
   
+    // console.log("Description" ,description);
     document.getElementById('category').value = category;
    document.getElementById('description').value = description;
    document.getElementById('expenseAmount').value = expenseAmount;
